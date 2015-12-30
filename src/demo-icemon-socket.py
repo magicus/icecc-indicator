525846c09ae613aca62784dbe85f9d9698774797
Magnus Ihse Bursie <mag@icus.se>
2015-12-30 01:51:40 +0100

    Update copyright and module description.

--- src/demo-icemon-socket.py
+++ src/demo-icemon-socket.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 
-import socket
+# iceccmonitor - a monitor for icecc/icecream in python
+#
+# Copyright (C) 2015 Magnus Ihse Bursie <mag@icus.se>
+#
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
 import errno
+import socket
 import struct
 import threading
 import time
-from datetime import datetime
 from collections import deque
+from datetime import datetime
 
 ################################################################################
 
@@ -301,6 +321,7 @@ class IceccMonitorProtocolHandler:
         if other_version < self.PROTOCOL_MIN_VERSION:
             raise IceccMonitorProtocolError('Server version too old')
         if other_version < self.PROTOCOL_MAX_VERSION:
+            # Update our version to use server's version
             self.protocol_version = other_version
 
     def confirm_version(self, version_packet):
